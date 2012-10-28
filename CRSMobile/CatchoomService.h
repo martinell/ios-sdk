5096a0e3dedfb0f4a9abc297e048bdfe4816e677
crisredfi <crisredfi1@gmail.com>
2012-10-28 11:41:03 +0000

    added sdk info

--- CRSMobile/CatchoomService.h
+++ CRSMobile/CatchoomService.h
@@ -18,11 +18,21 @@
 
 + (CatchoomService *)sharedCatchoom;
 
+// inits the server connection with catchoon. without this callback, the service will not work
+// this method must be called at the begining of the application.
 - (void)beginServerConnection;
-- (void)connect:(NSString *)token;
-- (void)search:(UIImage *)image;
-- (void)search:(UIImage*)image withToken:(NSString *)token;
 
+// Creates a connection with the server using a token. With this callback, you are autentificating the
+// application against catchoom service and connecting the app to a specific collection.
+// will answer with the delegate didReceiveConnectResponse: or didFailLoadWithError:
+- (void)connect:(NSString *)token;
+
+// Will create a search call for an image taken by the user.
+// will answer with a delegate didReceiveSearchResponse: or didFailLoadWithError:
+- (void)search:(UIImage *)image;
+
+
+// block helper to download a image from a URL (handy to create a lazy load).
 void UIImageFromURL( NSURL * URL, void (^imageBlock)(UIImage * image), void (^errorBlock)(void));
 
 @end
@@ -31,8 +41,16 @@ void UIImageFromURL( NSURL * URL, void (^imageBlock)(UIImage * image), void (^er
 @protocol CatchoomServiceProtocol <NSObject>
 
 @optional
+
+// delegate answer for connect callback. the return object is a JSON with the information of the
+// connection status
 - (void)didReceiveConnectResponse:(id)sender;
+
+// delegate answer for the Search callback. Ther return object is an Array of CatchoomSearchResponseItem(s)
+// the objects are already parsed and ready to use.
 - (void)didReceiveSearchResponse:(NSArray *)response;
+
+// delegate answer in case of answer error.
 - (void)didFailLoadWithError:(NSError *)error;
 
 @end
